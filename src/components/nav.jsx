c27d29cf54e5de22e7ae0aae9a214fa1b78f51ab
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-15 22:20:59 +0200

    eigth commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,7 +5,7 @@ export const Nav = () => {
         <>
         <div className="flex justify-center items-center">
           <nav className="h-20 bg-[#4d77ff] flex justify-between items-center px-4 w-6xl rounded-lg">
-            <img src="../src/assets/ok.png" alt="" className="h-50 w-50 object-contain"/>
+            <img src="/ok.png" alt="" className="h-50 w-50 object-contain"/>
             <div className="flex">
                 <ul className="flex gap-6 text-2xl text-white mr-8">
                     <li className="hover:text-green-500 transition-all duration-300 cursor-pointer font-medium">Home</li>
