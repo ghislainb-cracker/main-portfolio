c5639e24a1beb77ccd08b0a89d405000d2b99f8f
Byimbo Ghsilain <byimbog250@gmail.com>
2026-08-08 09:43:55 +0200

    responsive test21

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 
 export default function Main(){
